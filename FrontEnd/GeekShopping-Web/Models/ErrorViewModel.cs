dc028c19de1dd926d1cae1e5ae0e51f0168aa3b9
Talitha Souza <litha06h@gmail.com>
2025-06-03 21:11:03 -0300

    criando os primeiros microsservices para front e back e instalados os pacotes back-end necessários

---
+++ FrontEnd/GeekShopping-Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace GeekShopping_Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
