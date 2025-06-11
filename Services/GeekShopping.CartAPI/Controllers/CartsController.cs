a2dec2a8437383bbe6230ebef61eb01a16a444cd
Talitha Souza <litha06h@gmail.com>
2025-06-11 07:58:55 -0300

    controller pra cart

---
+++ Services/GeekShopping.CartAPI/Controllers/CartsController.cs
@@ -0,0 +1,64 @@
+
+using GeekShopping.CartAPI.DTO;
+using GeekShopping.CartAPI.Repository.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.CartAPI.Controllers
+{
+
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class CartsController : ControllerBase
+    {
+        private readonly ICartRepository _repository;
+
+        public CartsController(ICartRepository repository)
+        {
+            _repository = repository;
+        }
+
+
+       [HttpGet("find-cart/{id}")]
+        public async Task<ActionResult> FindById(string userId)
+        {
+            var cart = await _repository.FindCartByUserId(userId);
+
+            if (cart == null)
+                return NotFound();
+
+            return Ok(cart);
+        }
+
+        [HttpPost("add-cart/{id}")]
+        public async Task<ActionResult> AddCart(CartDTO DTO)
+        {
+            var cart = await _repository.SaveOrUpdateCart(DTO);
+
+            if (cart == null)
+                return NotFound();
+            return Ok(cart);
+        }
+
+        [HttpPut("update-cart/{id}")]
+        public async Task<ActionResult> UpdateCart(CartDTO DTO)
+        {
+            var cart = await _repository.SaveOrUpdateCart(DTO);
+
+            if (cart == null)
+                return NotFound();
+            return Ok(cart);
+        }
+
+        [HttpDelete("remove-cart/{id}")]
+        public async Task<ActionResult> RemoveCart(int id)
+        {
+            var status = await _repository.RemoveFromCart(id);
+
+            if (!status)
+                return BadRequest();
+
+            return Ok(status);
+        }
+    }
+}
