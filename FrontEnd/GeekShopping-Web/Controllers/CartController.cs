7de76fbf02093f69845d9f89cf4ad3766b3ebd2a
Talitha Souza <litha06h@gmail.com>
2025-06-20 16:16:20 -0300

    feat: funcionalidade de remover e aplicar cupom

--- FrontEnd/GeekShopping-Web/Controllers/CartController.cs
+++ FrontEnd/GeekShopping-Web/Controllers/CartController.cs
@@ -23,6 +23,38 @@ namespace GeekShopping_Web.Controllers
         {
             return View(await FindUserCart());
         }
+        
+        [HttpPost]
+        [ActionName("ApplyCoupon")]
+        public async Task<IActionResult> ApplyCoupon(CartViewModel model)
+        {
+            var token = await HttpContext.GetTokenAsync("access_token");
+            var userId = User.Claims.Where(u => u.Type == "sub")?.FirstOrDefault()?.Value;
+
+            var response = await _cartService.ApplyCoupon(model, token);
+
+            if (response)
+            {
+                return RedirectToAction(nameof(CartIndex));
+            }
+            return View();
+        }
+        
+        [HttpPost]
+        [ActionName("RemoveCoupon")]
+        public async Task<IActionResult> RemoveCoupon()
+        {
+            var token = await HttpContext.GetTokenAsync("access_token");
+            var userId = User.Claims.Where(u => u.Type == "sub")?.FirstOrDefault()?.Value;
+
+            var response = await _cartService.RemoveCoupon(userId, token);
+
+            if (response)
+            {
+                return RedirectToAction(nameof(CartIndex));
+            }
+            return View();
+        }
 
         public async Task<IActionResult> Remove(int id)
         {
@@ -31,7 +63,7 @@ namespace GeekShopping_Web.Controllers
 
             var response = await _cartService.RemoveFromCart(id, token);
 
-            if(response)
+            if (response)
             {
                 return RedirectToAction(nameof(CartIndex));
             }
@@ -49,7 +81,7 @@ namespace GeekShopping_Web.Controllers
             {
                 foreach (var detail in response.CartDetails)
                 {
-                    response.CartHeader.PurchaseAmount += (detail.Product.Price * detail.Count);
+                    response.CartHeader.DiscountAmount += (detail.Product.Price * detail.Count);
                 }
             }
             return response;
