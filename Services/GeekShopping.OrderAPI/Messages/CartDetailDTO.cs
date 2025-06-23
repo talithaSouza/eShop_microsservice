0c38de9f516e7ab437e6b22f149a0ab16879c0ec
Talitha Souza <litha06h@gmail.com>
2025-06-23 16:44:18 -0300

    feat: DTOs

---
+++ Services/GeekShopping.OrderAPI/Messages/CartDetailDTO.cs
@@ -0,0 +1,13 @@
+namespace GeekShopping.OrderAPI.Messages
+{
+    public class CartDetailDTO
+    {
+        public long Id { get; set; }
+        public long CartHeaderId { get; set; }
+
+        public long CartId { get; set; }
+        public long ProductId { get; set; }
+        public int Count { get; set; }
+        public virtual ProductDTO Product { get; set; }
+    }
+}
