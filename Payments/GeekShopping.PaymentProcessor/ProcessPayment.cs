ca1d2cdc448998e8095033f88cb0bd26e7607832
Talitha Souza <litha06h@gmail.com>
2025-06-26 21:26:13 -0300

    feat: Criacao no projeto de biblioteca PaymentProcessor

---
+++ Payments/GeekShopping.PaymentProcessor/ProcessPayment.cs
@@ -0,0 +1,11 @@
+namespace GeekShopping.PaymentProcessor
+{
+    public class ProcessPayment : IProcessPayment
+    {
+        public bool PaymentProcessor()
+        {
+            return true;
+        }
+
+    }
+}
