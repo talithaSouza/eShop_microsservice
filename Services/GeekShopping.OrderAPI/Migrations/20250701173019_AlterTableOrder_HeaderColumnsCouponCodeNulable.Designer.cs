053b3a51b15995de80c0d649822517587299d0fb
Talitha Souza <litha06h@gmail.com>
2025-07-01 14:33:46 -0300

    refactor: alterado coluna coupon_code para nulable

--- Services/GeekShopping.OrderAPI/Migrations/20250701173019_AlterTableOrder_HeaderColumnsCouponCodeNulable.Designer.cs
+++ Services/GeekShopping.OrderAPI/Migrations/20250701173019_AlterTableOrder_HeaderColumnsCouponCodeNulable.Designer.cs
@@ -4,6 +4,7 @@ using GeekShopping.OrderAPI.Model.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GeekShopping.OrderAPI.Migrations
 {
     [DbContext(typeof(MySqlContext))]
-    partial class MySqlContextModelSnapshot : ModelSnapshot
+    [Migration("20250701173019_AlterTableOrder_HeaderColumnsCouponCodeNulable")]
+    partial class AlterTableOrder_HeaderColumnsCouponCodeNulable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
